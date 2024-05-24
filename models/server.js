e5d8c88d8423c8c56ff81d9df1368e78a3dbf3ff
daviddexteel <dsilvestri@dexteel.com>
2024-05-24 19:06:55 -0300

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,41 @@
+
+const express = require('express'); //Express server
+const http = require('http'); // Sockets Server
+const scoketio = require('socket.io');
+const path = require('path');
+const Sockets = require('./sockets');
+
+
+class Server {
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+
+        //Http server
+        this.server = http.createServer(this.app);
+        //Socket configuration
+        this.io = scoketio(this.server,{ /* configutations */ });
+    }
+
+    middlewares(){
+        this.app.use(express.static( path.resolve(__dirname, '../public')));
+        
+    }
+
+    configureSockets(){
+        new Sockets(this.io);
+    }
+
+    execute(){
+        // initialize middlewares
+        this.middlewares();
+        // initialize sockets
+        this.configureSockets();
+        // initialize server
+        this.server.listen(this.port, () => {
+            console.log('Server running in Port:',this.port);
+        })
+    }
+}
+
+module.exports = Server;
